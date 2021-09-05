428b781952c80b2acbb0e6f1759af73780cb6dec
Aziz Bayu Pratama <azizbp6@gmail.com>
2021-09-05 11:21:48 +0700

    menambahkan h1 dan pargaraf pada index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores aliquid eaque libero nobis! Quo voluptatibus iste laborum repellat dolores explicabo perferendis sint a impedit at numquam veritatis, consequatur reiciendis qui.</p>
 </body>
 </html>
